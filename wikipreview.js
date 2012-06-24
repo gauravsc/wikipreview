cfb45af35cb2f72a63d40fe3accdd69250f2d4af
Gaurav Singh <gauravonline20@gmail.com>
2012-06-24 11:26:07 +0530

    final plugin commited

--- wikipreview.js
+++ wikipreview.js
@@ -1,28 +1,34 @@
+$.fn.wpreview = function(options) {
+var wikipreview =function(leftp,topp,text){
 
+	$.getJSON('http://en.wikipedia.org/w/api.php?action=query&list=search&srsearch='+text+'&srprop=timestamp&format=json&callback=?', function(data) {
+	if(data.query.search.length!==0){
+		var url='http://en.wikipedia.org/wiki/'+data.query.search[0].title ;	 
+	 	if($('iframe#preview').length===0){   
+	 		$('<iframe src="'+ url +'"  id="preview" frameborder="1" style="width:600px;height:300px;z-index:40;position:absolute;"></iframe>').appendTo('body');
+         		$('iframe#preview').css({'top':topp, 'left':leftp});
+		}else {
+	 		$('iframe#preview').attr('src','http://en.wikipedia.org/wiki/'+data.query.search[0].title);
+	 		$('iframe#preview').css('display','block');
+			}
+		}
+	});
+}
 
-<html>
-<head>
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-
-
-$.getJSON('http://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=macbook ipad&srprop=timestamp&format=json&callback=?', function(data) {
-if(data.query.search.length===0){
-alert("no result found");
-}  else{
-    
- $('iframe#preview').attr('src','http://en.wikipedia.org/wiki/'+data.query.search[0].title);
-    
+var clearpreview= function(){
+	$('iframe#preview').css('display','none');
 
 }
 
+if(options.func=="wikipreview")
+	wikipreview(options.left,options.top,options.text);
+else
+	clearpreview();
+};
 
-});​
-
-
-<body>
 
 
 
-</body>
 
-</html>
+
+
